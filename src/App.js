f92aab7479dbd25f39545dfdb337d2a3731321b2
Anamya Vats <121956516+Anamya01@users.noreply.github.com>
2024-06-08 23:11:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Desc from './Desc/Desc';
+import Footer from './Footer/Footer';
+import Main from './Main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+      <Desc/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
